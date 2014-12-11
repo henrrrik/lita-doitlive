b69e870c501769813bfbd1e599c90760b95bc424
Henrik Sjökvist <henrik.sjokvist@gmail.com>
2014-12-11 13:10:36 +0100

    Update to support Lita 4.

--- spec/lita/handlers/doitlive_spec.rb
+++ spec/lita/handlers/doitlive_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe Lita::Handlers::Doitlive, lita_handler: true do
-  it { routes_command("do it live").to(:do_it_live) }
-  it { routes_command("DO IT LIVE").to(:do_it_live) }
-  it { routes_command("we'll do it live!").to(:do_it_live) }
+  it { is_expected.to route_command("do it live").to(:do_it_live) }
+  it { is_expected.to route_command("DO IT LIVE").to(:do_it_live) }
+  it { is_expected.to route_command("we'll do it live!").to(:do_it_live) }
 end
